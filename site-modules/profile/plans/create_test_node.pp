24dfc30e8012c206a061f28ef5a67c9c2ba26057
Eric Zounes <eric.zounes@puppet.com>
2025-04-18 12:31:17 -0700

    Add plan to create test node

---
+++ site-modules/profile/plans/create_test_node.pp
@@ -0,0 +1,8 @@
+plan profile::create_test_node () {
+  $provision_response = run_task('provision::abs', 'pe-ai-assistant-demo-1-prod.c.team-cd4pe-scratchpad.internal', {
+      'action'   => 'provision',
+      'platform' => 'ubuntu-2004-x86_64-pooled',
+  })
+  out::message($provision_response)
+  out::message(get_targets('all'))
+}
