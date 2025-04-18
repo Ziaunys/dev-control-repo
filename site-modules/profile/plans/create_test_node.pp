f5fbf9cf0dc9dd6eed5ea20deb1ffb256ddf42a0
Eric Zounes <eric.zounes@puppet.com>
2025-04-18 16:27:02 -0700

    Make plan install Puppet on test node

--- site-modules/profile/plans/create_test_node.pp
+++ site-modules/profile/plans/create_test_node.pp
@@ -4,4 +4,20 @@ plan profile::create_test_node () {
       'platform' => 'ubuntu-2004-x86_64-pooled',
   })
   out::message($provision_response)
+  $test_node = $provision_response[0].value['nodes'][0]
+  $target_config = {
+    'name' => $test_node['hostname'],
+    'config' => {
+      'transport' => 'ssh',
+      'ssh' => {
+        'user'           => 'root',
+        'host-key-check' => false,
+        'private-key'    => '/opt/puppetlabs/server/data/orchestration-services/id_rsa',
+      },
+    },
+  }
+  $test_target = Target.new($target_config)
+  run_command('curl -k https://pe-ai-assistant-demo-1-prod.c.team-cd4pe-scratchpad.internal:8140/packages/current/install.bash | sudo bash', $test_target)
+  run_command('puppet agent -t', $test_target)
+  return $test_node['hostname']
 }
